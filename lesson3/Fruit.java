59f215166b02261cb1d285bdcb60f6c7b24db5c3
nagval666 <grasin999@gmail.com>
2022-02-26 18:27:55 +0300

    Home Work 3

---
+++ lesson3/Fruit.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public abstract class Fruit {
+
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ lesson3/Orange.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+public class Orange extends Fruit{
+
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "Апельсины";
+    }
+}
