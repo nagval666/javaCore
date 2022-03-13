db865f0468b2ae47f99b0d59f58469700443e6d8
nagval666 <grasin999@gmail.com>
2022-03-13 15:18:25 +0300

    Home work 5

---
+++ lesson5/AppData.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+public class AppData {
+
+    private String[] header;
+    private int[][] data;
+
+    public AppData(String[] header,int[][] data){
+        this.header = header;
+        this.data = data;
+    }
+
+    public String[] getHeader(){return header;}
+    public int[][] getData(){return data;}
+}
